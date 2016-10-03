d294066f65f1019a15547f346fcbb960aa9e33ba
Cassian Lup <cassi.lup@gmail.com>
2016-10-04 01:20:43 +0300

    connect chatService to the node backend

--- client/src/app/shared/chat.service.ts
+++ client/src/app/shared/chat.service.ts
@@ -1,29 +1,40 @@
 import { Injectable } from '@angular/core';
 import { Message } from '../types';
+import { Observable } from 'rxjs';
+import * as io from 'socket.io-client';
 
 @Injectable()
 export class ChatService {
-  messages:Message[];
+  private url = 'http://localhost:3000';
+  private socket;
+  private messages:Message[] = [];
+  private connection;
 
   constructor() {
-    this.messages = [{
-      text: "first message"
-    }, {
-      text: "2nd message"
-    }, {
-      text: "3rd message"
-    }, {
-      text: "4th message"
-    },{
-      text: "5th message"
-    }];
-  }
+    this.socket = io(this.url);
 
-  public sendMessage(text:string) {
-    this.messages.push({ text });
+    this.connection = this.onMessageReceived().subscribe((message:string) => {
+      this.messages.push({ text: message });
+    });
   }
 
   public getMessages():Array<Message> {
     return this.messages;
+  }
+
+  sendMessage(message){
+    this.socket.emit('chat message', message);
+  }
+
+  onMessageReceived() {
+    let observable = new Observable(observer => {
+      this.socket.on('chat message', (data) => {
+        observer.next(data);
+      });
+      return () => {
+        this.socket.disconnect();
+      };
+    })
+    return observable;
   }
 }
