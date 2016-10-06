9e6fb5ca17a83109c414200c484fce984368f56c
Cassian Lup <cassi.lup@gmail.com>
2016-10-07 02:33:52 +0300

    cleaning up

--- 2-final/client/src/bootstrap.ts
+++ 2-final/client/src/bootstrap.ts
@@ -1,8 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
-var styles = require('./styles.css');
-
 const platform = platformBrowserDynamic();
-
 platform.bootstrapModule(AppModule);
